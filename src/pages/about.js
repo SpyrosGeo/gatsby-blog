b59f16959777131b34d649e563688dbb85bb959e
SpyrosGeo <xarodasimba@gmail.com>
2021-09-14 02:21:51 +0300

    finished Gatsby tutorial pt 2

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Link} from 'gatsby'
+import Layout from '../components/layout'
+
+
+
+
+const About = () => {
+//  const handleClick = ()=>{
+//      history.back()
+//  }
+    return (
+       <Layout pageTitle="About me..">
+           <p>I'ts me.. the about page again..</p>
+       </Layout> 
+    )
+}
+export default About
