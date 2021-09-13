d57a5216e1c15a56d777d8d3dc49cbe8b6b71c26
SpyrosGeo <xarodasimba@gmail.com>
2021-09-14 02:34:23 +0300

    pt 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import Layout from '../components/layout'
-
+import { StaticImage } from "gatsby-plugin-image"
 
 
 
@@ -9,6 +9,14 @@ const IndexPage = () =>{
     <Layout pageTitle="Home?">
 
       <p>Something..Something.. Gatsby </p>
+      <StaticImage 
+      alt="Some kind of dog.. I'dont know what I'm doing anymore..."
+      src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large">
+      </StaticImage>
+      <StaticImage 
+      alt="Some kind of dog.. I'dont know what I'm doing anymore..."
+      src="../images/dog.jpeg">
+      </StaticImage>
     </Layout>
   )
 }
